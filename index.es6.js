a8166a1da1cb53485dd8f8a776300cece38cece2
ncarson <17229021+NCarson@users.noreply.github.com>
2018-12-11 19:39:23 -0800

    added named imports to index.js

---
+++ index.es6.js
@@ -0,0 +1,6 @@
+import PostgrestFetcher from './lib/postgrest-fetcher.js'
+export {PostgrestFetcher}
+
+import PostgrestQuery from './lib/postgrest-query.js'
+export {PostgrestQuery}
+
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-import PostgrestFetcher from 
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.PostgrestQuery = exports.PostgrestFetcher = undefined;
+
+var _postgrestFetcher = require('./lib/postgrest-fetcher.js');
+
+var _postgrestFetcher2 = _interopRequireDefault(_postgrestFetcher);
+
+var _postgrestQuery = require('./lib/postgrest-query.js');
+
+var _postgrestQuery2 = _interopRequireDefault(_postgrestQuery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.PostgrestFetcher = _postgrestFetcher2.default;
+exports.PostgrestQuery = _postgrestQuery2.default;
+
